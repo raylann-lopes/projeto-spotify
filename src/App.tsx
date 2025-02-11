927b29e31ee2ddfd78f1ec0291c94184de5e2bbe
raylannlopes <raylannlopes@gmail.com>
2025-02-11 20:35:32 -0300

    initialize React project with Vite, TypeScript, and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "tailwindcss";
+import Header from "./components/header";
+
+const App = () => {
+  return <Header />;
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoSpotify from "../assets/logo/spotify-logo.png";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={LogoSpotify} alt="LogoSpotify" />
+    </div>
+  );
+};
+export default Header;
