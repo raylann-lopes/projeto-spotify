7043f6bb61baa2397c59be4373dafd4c929e9fa5
raylannlopes <raylannlopes@gmail.com>
2025-02-13 21:39:37 -0300

    feat: refactor ItemList and Main components to use path and idPath props for improved routing

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -12,32 +12,32 @@ interface ItemListProps {
     id: number;
     banner?: string;
   }[];
-  itemsMore: string;
-  idItemsMore: string;
+  path: string;
+  idPath: string;
 }
 
 const ItemList: React.FC<ItemListProps> = ({
   title,
   items,
   itemsArray,
-  itemsMore,
-  idItemsMore,
+  path,
+  idPath,
 }) => {
   return (
     <>
       <div className="flex flex-col gap-2.5 p-0">
         <div className="flex items-center justify-between leading-normal">
           <h2 className="text-2xl">{title} Populares</h2>
-          <Link to={itemsMore} className="hover:underline">
+          <Link to={path} className="hover:underline">
             Mostrar tudo
           </Link>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-9">
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5">
           {itemsArray
             .filter((currentValue, index) => index < items.length)
             .map((currentObject, index) => (
               <SingleItem
-                idItemsMore={idItemsMore}
+                idPath={idPath}
                 {...currentObject}
                 key={`${title}-${index}`}
               />
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,27 +2,39 @@ import artistArray from "../assets/database/artists";
 import songsArray from "../assets/database/songs";
 import ItemList from "./ItemList";
 
-const Main = () => {
+interface MainProps {
+  type: "artists" | "songs" | undefined;
+}
+
+const Main = ({ type }: MainProps) => {
   return (
     <>
       <div className=" bg-gradient-to-b from-[#062d14] to-black rounded-2xl flex p-5 flex-col justify-between mt-0 ml-2.5 mr-2.5 mb-2.5 gap-10 text-white">
         {/* Artist List */}
-        <ItemList
-          title="Artistas"
-          items={Array(5).fill(null)}
-          itemsArray={artistArray}
-          itemsMore="/artists"
-          idItemsMore="/artist"
-        />
+        {type === "artists" || type === undefined ? (
+          <ItemList
+            title="Artistas"
+            items={Array(10).fill(null)}
+            itemsArray={artistArray}
+            path="/artists"
+            idPath="/artist"
+          />
+        ) : (
+          <></>
+        )}
 
         {/* Music List */}
-        <ItemList
-          title="Musicas"
-          items={Array(10).fill(null)}
-          itemsArray={songsArray}
-          itemsMore="/songs"
-          idItemsMore="/song"
-        />
+        {type === "songs" || type === undefined ? (
+          <ItemList
+            title="Musicas"
+            items={Array(20).fill(null)}
+            itemsArray={songsArray}
+            path="/songs"
+            idPath="/song"
+          />
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
