7b6719e4ea0764f37c1ae7a5d8f98dc3d8e979f5
raylannlopes <raylannlopes@gmail.com>
2025-02-14 13:39:40 -0300

    feat: add PlayButton component with play icon

---
+++ src/components/PlayButton.tsx
@@ -0,0 +1,12 @@
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const button = () => {
+  return (
+    <>
+      <FontAwesomeIcon icon={faCirclePlay} />
+    </>
+  );
+};
+
+export default button;
