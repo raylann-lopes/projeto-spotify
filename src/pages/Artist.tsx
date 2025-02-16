330e7e856013236ee9cdd5013ed477e9e93d35ec
raylannlopes <raylannlopes@gmail.com>
2025-02-16 15:29:41 -0300

    feat: enhance Artist and Song pages with dynamic data and improved layout

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -1,19 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import PlayButton from "../components/PlayButton";
 import SongList from "../components/SongList";
+import artistArray from "../assets/database/artists";
+import songArray from "../assets/database/songs";
 
 const Artist = () => {
+  const { id } = useParams();
+
+  const { name, banner } = artistArray.filter(
+    (currentArtistObj) => currentArtistObj.id === Number(id)
+  )[0];
+
+  const songsArrayFromArtist = songArray.filter(
+    (currentSongObj) => currentSongObj.artist === name
+  );
+
+  const randomIndex = Math.floor(
+    Math.random() * (songsArrayFromArtist.length - 1)
+  );
+
+  const randomIdFromArtist = songsArrayFromArtist[randomIndex].id;
+
+  console.log(randomIdFromArtist);
+
   return (
     <>
-      <div className="mt-0 mr-2.5 mb-2.5 ml-2.5 overflow-hidden">
+      <div className="mt-0 mr-2.5 mb-2.5 ml-2.5 overflow-auto">
         <div
           className="flex rounded-2xl items-end p-6 h-[40svh] bg-cover bg-[position:y-20%]"
           style={{
-            backgroundImage:
-              "linear-gradient(to bottom, var(--_shade), var(--_shade)),url(https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607)",
+            backgroundImage: `linear-gradient(to bottom, var(--_shade), var(--_shade)),url(${banner})`,
           }}
         >
-          <h2 className="text-white text-8xl">Jorge & Matheus</h2>
+          <h2 className="text-white text-8xl">{name}</h2>
         </div>
 
         <div
@@ -24,11 +43,11 @@ const Artist = () => {
           }}
         >
           <h2>Populares</h2>
-          <SongList />
+          <SongList songsArray={songsArrayFromArtist} />
         </div>
 
-        <Link to="/artist/1">
-          <div className="fixed opacity-1 transform translate-y-0 right-6 bottom-6">
+        <Link to={`/song/${randomIdFromArtist}`}>
+          <div className="text-5xl fixed opacity-100 transform translate-y-0 right-8 bottom-8 text-green-400 hover:transform  ease duration-200 hover:text-gren-600 hover:text-6xl">
             <PlayButton />
           </div>
         </Link>
--- src/pages/Song.tsx
+++ src/pages/Song.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Play from "../components/Play";
+import songsArray from "../assets/database/songs";
+import artistArray from "../assets/database/artists";
 
 const Song = () => {
-  return <div>Song</div>;
+  const { id } = useParams();
+
+  const { image, name, duration, artist, audio } = songsArray.filter(
+    (currentSongObj) => currentSongObj.id === Number(id)
+  )[0];
+
+  const artistObj = artistArray.filter(
+    (currentArtistObj) => currentArtistObj.name === artist
+  )[0];
+
+  return (
+    <div className="grid grid-rows-1 flex-1">
+      <div
+        className="my-0 mx-5 rounded-tr-2xl rounded-tl-2xl flex justify-center items-center p-6"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, var(--green-800), var(--green-950))",
+        }}
+      >
+        <div className="shadow-(--_shadow)">
+          <img src={image} alt={`Image da Musica ${name}`} />
+        </div>
+      </div>
+      <div className="grid grid-cols-[20%_60%_20%] place-items-center bg-black text-white gap-4 py-4 px-6">
+        <div className="justify-self-start rounded-l overflow-hidden">
+          <Link to={`/artist/${artistObj.id}`}>
+            <img
+              width={75}
+              height={75}
+              src={artistObj.image}
+              alt={`Image do artista ${artistObj.name}`}
+            />
+          </Link>
+        </div>
+        <Play duration={duration} />
+        <div>
+          <p className="text-lg font-bold">{name}</p>
+          <p>{artistObj.name}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Song;
